91133198e5a6c6a6adb61a34e74a2e1d3703a8cd
Elena Vereskun <lena.vereskun@yandex.ru>
2022-12-14 19:50:21 +0300

    feature/profile

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 const editButton = document.querySelector('.profile__edit-button');
-const openedPopup = document.querySelector('.popup_opened');
+const openedPopup = document.querySelector('.popup__close-button');
 
 editButton.addEventListener('click', (event) => {
     event.preventDefault();
-    console.log(event);
+    console.log('event');
     openedPopup.classList.add('.popup_opened');
 })
