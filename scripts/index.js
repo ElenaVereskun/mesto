2663815435cd5e008c794792e5d838db17de0535
Elena Vereskun <lena.vereskun@yandex.ru>
2022-12-19 21:21:40 +0300

    popup/close/opened

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,49 @@
 const editButton = document.querySelector('.profile__edit-button');
-const openedPopup = document.querySelector('.popup');
-const closeButton = openedPopup.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.popup__close-button');
+const saveButton = popup.querySelector('.popup__save-button');
 
 editButton.addEventListener('click', (event) => {
     event.preventDefault();
     console.log(event);
-    openedPopup.classList.add('.popup_opened');
-})
-
-closeButton.addEventListener('click', () => {
-    openedPopup.classList.remove('.popup_opened');
+    popup.classList.add('.popup_opened');
 });
 
-editButton.addEventListener('click', (event) => {
-console.log(event.target, event.currentTarget);
-/* openedPopup.classList.remove('.popup_opened'); */
-})
+closeButton.addEventListener('click', () => {
+    popup.classList.remove('.popup_opened');
+});
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let formElement = document.querySelector('.profile');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    console.log(popupName.value);
+    console.log(popupAbout.value);
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    let popupName = popup.querySelector('.popup__name');
+    let popupAbout = popup.querySelector('.popup__about');
+    // Выберите элементы, куда должны быть вставлены значения полей
+    console.log(profileName.textContent);
+    console.log(profileAbout.textContent);
+    // Вставьте новые значения с помощью textContent
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
