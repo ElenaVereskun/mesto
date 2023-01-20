97f7a820c0c8047ff409f9d3a6fce193e66b94b0
Elena Vereskun <lena.vereskun@yandex.ru>
2023-01-20 15:45:07 +0300

    ppixel

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,12 @@
 const popup = document.querySelector('.popup');
-const buttonEdit = popup.querySelector('.profile__edit-button');
+const buttonEdit = document.querySelector('.profile__edit-button');
 const buttonClose = popup.querySelector('.popup__close-button');
 let popupForm = document.querySelector('.popup__form');
-let popupName = popupForm.querySelector('popup_user-info_name');
-let popupJob = popupForm.querySelector('popup_user-info_job');
+let popupName = popupForm.querySelector('.popup_user-info_name');
+let popupJob = popupForm.querySelector('.popup_user-info_job');
 let profile = document.querySelector('.profile');
 let profileName = profile.querySelector('.profile__name');
 let profileJob = profile.querySelector('.profile__job');
-
-function hendler() {
-    profileName.textContent;
-    profileJob.textContent;
-
-    profileName.textContent = popupName.value;
-    profileJob.textContent = popupJob.value;
-}
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
@@ -27,13 +19,17 @@ function handleFormSubmit(evt) {
 
     profileName.textContent = popupName.value;
     profileJob.textContent = popupJob.value;
+
+    /* buttonClose.addEventListener('click', handleFormSubmit); */
+    //значения из профайла появляются в импутах
 }
 popupForm.addEventListener('submit', handleFormSubmit);
 
-buttonEdit.addEventListener('click', hendler => {
+buttonEdit.addEventListener('click', () => {
     popup.classList.add('popup_opened');
+    handleFormSubmit();
 });
 
-buttonClose.addEventListener('click', hendler => {
+buttonClose.addEventListener('click', () => {
     popup.classList.remove('popup_opened');
 });
